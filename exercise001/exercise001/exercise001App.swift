ce3a4a9ca0fecc65782c45dae88270889080dc7f
Panos Vakalis <dev.panagiotis.vakalis@outlook.com>
2025-12-06 13:17:29 +0200

    Initial commit

---
+++ exercise001/exercise001/exercise001App.swift
@@ -0,0 +1,17 @@
+//
+//  exercise001App.swift
+//  exercise001
+//
+//  Created by Panagiotis Vakalis on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct exercise001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
