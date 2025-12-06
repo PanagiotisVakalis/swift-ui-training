c06ce54dbc3d3d7adbef04ba52d660959d3c854f
Panos Vakalis <dev.panagiotis.vakalis@outlook.com>
2025-12-06 13:19:37 +0200

    Rename ContentView to ParentCounterView

--- exercise001/exercise001/ParentCounterView.swift
+++ exercise001/exercise001/ParentCounterView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  ParentCounterView.swift
 //  exercise001
 //
 //  Created by Panagiotis Vakalis on 6/12/25.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ParentCounterView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ParentCounterView()
 }
