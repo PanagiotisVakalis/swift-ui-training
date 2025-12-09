755b49e7cb45527caf0d0798af899180c8dcc1aa
Panos Vakalis <dev.panagiotis.vakalis@outlook.com>
2025-12-09 20:10:46 +0200

    Allow the ViewModel to select an item

--- exercise002/exercise002/ViewModel.swift
+++ exercise002/exercise002/ViewModel.swift
@@ -10,11 +10,16 @@ import Combine
 
 final class ViewModel: ObservableObject {
     @MainActor @Published var items: [String] = []
+    @Published var selectedItem: String? = nil
     
     func loadItems() async {
         try? await Task.sleep(for: .seconds(2))
         await MainActor.run {
             self.items = ["Item 1", "Item 2", "Item 3"]
         }
+    }
+    
+    func selectItem(at index: Int) {
+        selectedItem = items[index]
     }
 }
