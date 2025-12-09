75406cd65eceb9dc6061171a7f6dd27b431eeaab
Panos Vakalis <dev.panagiotis.vakalis@outlook.com>
2025-12-09 19:38:54 +0200

    Rename the ContentView to ParentView

--- exercise002/exercise002/ParentView.swift
+++ exercise002/exercise002/ParentView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  ParentView.swift
 //  exercise002
 //
 //  Created by Panagiotis Vakalis on 9/12/25.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ParentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ParentView()
 }
