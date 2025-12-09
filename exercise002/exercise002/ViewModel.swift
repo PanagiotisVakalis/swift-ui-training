72dafbf6bbfa86a01322d78805c41719f2d69d83
Panos Vakalis <dev.panagiotis.vakalis@outlook.com>
2025-12-09 19:52:36 +0200

    Create the ViewModel

---
+++ exercise002/exercise002/ViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ViewModel.swift
+//  exercise002
+//
+//  Created by Panagiotis Vakalis on 9/12/25.
+//
+
+import Foundation
+import Combine
+
+final class ViewModel: ObservableObject {
+    @MainActor @Published var items: [String] = []
+    
+    func loadItems() async {
+        try? await Task.sleep(for: .seconds(2))
+        await MainActor.run {
+            self.items = ["Item 1", "Item 2", "Item 3"]
+        }
+    }
+}
